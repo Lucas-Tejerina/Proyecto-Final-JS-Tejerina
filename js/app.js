eb257b4ceb25969c0037fbd37b69444dd7346cf7
Lucas Tejerina <lucastejerina18@hotmail.com>
2024-06-28 19:59:07 -0300

    se agrego el algoritmo con ciclos

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 let pregunta = prompt ("Cual es tu color favorito?: \n A) El Rojo \n B) El Azul \n C) El Verde") .toLocaleUpperCase();
 
 if (pregunta === "A"){
-    alert("Ardiente como el fuego")
+    alert("Furioso como el fuego")
 }else if (pregunta === "B"){
     alert("Tranquilo como el mar")
 }else if (pregunta === "C") {
     alert("Revoltoso como la naturaleza")
 }else {
     alert("Ingresa una opcion correcta, por favor.")
+}
+
+
+let numero = parseInt(prompt("Por favor ingrese un numero POSITIVO para hacer la suma:"));
+
+if (isNaN(numero) || numero <= 0) {
+    alert("Ingresa un numero positivo valido, por favor.");
+} else {
+    let suma = 0;
+    let i = 1;
+
+    while (i <= numero) {
+        suma = suma + i;
+        i++;
+    }
+
+    alert("La suma de los números del 1 al " + numero + " es: " + suma);
 }
 
 

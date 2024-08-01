258d393dad823ec53b326488decac22f6ebc0755
Lucas Tejerina <lucastejerina18@hotmail.com>
2024-08-01 15:42:37 -0300

    se cambio el array de objetos por un class

--- js/app.js
+++ js/app.js
@@ -1,91 +1,22 @@
-const peliculas = [
-    {
-        titulo: "Intensamente",
-        genero: "Animacion",
-        anio: 2015,
-    },
-    {
-        titulo: "Elementos",
-        genero: "Animacion",
-        anio: 2023
-    },
-    {
-        titulo: "Saw",
-        genero: "Terror",
-        anio: 2004,
-    },
-    {
-        titulo: "La noche del demonio",
-        genero: "Terror",
-        anio: 2010
-    },
-    {
-        titulo: "Kung Fu Panda",
-        genero: "Comedia",
-        anio: 2008,
-    },
-    {
-        titulo: "¿Y donde estan las rubias?",
-        genero: "Comedia",
-        anio: 2004
-    },
-    {
-        titulo: "Tomb Raider",
-        genero: "Accion",
-        anio: 2001,
-    },
-    {
-        titulo: "Los vengadores",
-        genero: "Accion",
-        anio: 2012
-    },
-    {
-        titulo: "Wonka",
-        genero: "Musicales",
-        anio: 2023,
-    },
-    {
-        titulo: "La la land",
-        genero: "Musicales",
-        anio: 2016
+class Peliculas {
+    constructor(titulo, genero, anio) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anio = anio;
     }
+}
+
+const peliculas = [
+    new Peliculas("Intensamente", "Animacion", 2015),
+    new Peliculas("Elementos", "Animacion", 2023),
+    new Peliculas("Saw", "Terror", 2004),
+    new Peliculas("La noche del demonio", "Terror", 2010),
+    new Peliculas("Kung fu Panda", "Comedia", 2008),
+    new Peliculas("¿Y donde estan las rubias?", "Comedia", 2004),
+    new Peliculas("Tomb Raider", "Accion", 2001),
+    new Peliculas("Los Vengadores", "Accion", 2012),
+    new Peliculas("Wonka", "Musicales", 2016),
+    new Peliculas("La la land", "Musicales", 2016)
 ];
 
-function mostrarPeliculas(generoSeleccionado) {
-    peliculas.filter((pelicula) => {
-        if (pelicula.genero === generoSeleccionado) {
-            console.log(`${pelicula.titulo} es una película del género ${pelicula.genero} estrenada en el año ${pelicula.anio}`);
-        }
-    });
-}
 
-
-let opcion = prompt("¿Qué género de película te gusta más?\nA) Animacion\nB) Terror\nC) Comedia\nD) Accion\nE) Musicales\nF) Películas disponibles").toUpperCase();
-
-
-switch (opcion) {
-    case "A":
-        mostrarPeliculas("Animacion"); 
-        break;
-    case "B":
-        mostrarPeliculas("Terror"); 
-        break;
-    case "C":
-        mostrarPeliculas("Comedia"); 
-        break;
-    case "D":
-        mostrarPeliculas("Accion"); 
-        break;
-    case "E":
-        mostrarPeliculas("Musicales"); 
-        break;
-    case "F":
-        console.log("Películas disponibles:");
-        peliculas.forEach((pelicula) => {
-            console.log(`${pelicula.titulo} - ${pelicula.genero} (${pelicula.anio})`);
-        });
-        break;
-    default:
-        console.log("Ingresa una opción válida");
-        break;
-}
